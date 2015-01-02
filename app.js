06d59343184705de9eb554f0566d8b43d6c227a5
conorhastings <hastings.conorm@gmail.com>
2015-01-02 17:41:04 -0500

    very basic working version

--- app.js
+++ app.js
@@ -1,11 +1,45 @@
 var express = require('express');
 var app = express();
 var _ = require('underscore');
+var fs = require('fs')
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
 	res.sendFile('/public/index.html', {"root": __dirname});
 });
+
+app.get('/nba', function(req, res){
+	var birthday = new Date(req.query.birthday);
+	fs.readFile('./public/nba_players.json', 'utf8', function (err,data) {
+		if (err) {
+			return console.log(err);
+		}
+		totalPlayers = JSON.parse(data);
+		youngerPlayers = [];
+		_.each(totalPlayers,function(player){
+			var playerBirthday = new Date(player);
+			if(birthday - playerBirthday < 0){
+				youngerPlayers.push(player)
+			}
+
+
+
+
+
+		})
+		console.log(youngerPlayers.length)
+		var percentageOlder = ((youngerPlayers.length/totalPlayers.length)*100).toFixed(2);
+		res.send({nba:percentageOlder});
+
+	});
+
+
+});
+
+
+
+
+
 
 var server = app.listen(3000, function(){
 	console.log('listening on *:3000');
