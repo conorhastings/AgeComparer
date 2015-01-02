b58c11fb19ccd86fdf75316c32b02a81a253bd15
conorhastings <hastings.conorm@gmail.com>
2015-01-02 13:10:59 -0500

    currently console logging what I believe to be the relevant information for nba players

--- nba_scraper.js
+++ nba_scraper.js
@@ -6,5 +6,6 @@ request('http://www.basketball-reference.com/friv/birthdays.cgi?month=1&day=1', 
 	if (!error && response.statusCode == 200) {
 		$ = cheerio.load(body);
 		console.log($('.sortable').find('tr')[8].children[3].attribs.class.trim() === 'bold_text')
+		console.log($('.sortable').find('tr')[8].children[5])
 	}
 })
