10255e85f2bfeb4e774bb61649a28df57fd597b6
conorhastings <hastings.conorm@gmail.com>
2015-01-03 22:45:22 -0500

    nfl now included on front end, many changes to server side, created function for determining percentage, using async.js library to handle multiple fs.readFiles

--- app.js
+++ app.js
@@ -2,68 +2,55 @@ var express = require('express');
 var app = express();
 var _ = require('underscore');
 var fs = require('fs');
+var async = require('async');
 
 var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
+
+
+function percentageOlder(totalPlayers, birthday){
+	var youngerPlayers = [];
+	_.each(totalPlayers,function(player){
+		var playerBirthday = new Date(player);
+		if(birthday - playerBirthday < 0){
+			youngerPlayers.push(player);
+		}
+	})
+	var percentageOlder = ((youngerPlayers.length/totalPlayers.length)*100).toFixed(2);
+	return percentageOlder;
+};
+
 
 app.get('/', function(req, res){
 	res.sendFile('/public/index.html', {"root": __dirname});
 });
 
-app.get('/nba', function(req, res){
+app.get('/agedata', function(req, res){
 	var birthday = new Date(req.query.birthday);
-	fs.readFile('./public/nba_players.json', 'utf8', function (err,data) {
-		if (err) {
-			return console.log(err);
-		}
-		totalPlayers = JSON.parse(data);
-		youngerPlayers = [];
-		_.each(totalPlayers,function(player){
-			var playerBirthday = new Date(player);
-			if(birthday - playerBirthday < 0){
-				youngerPlayers.push(player)
+
+	var files = ['./public/nba_players.json','./public/nfl_players.json']
+
+	async.map(files, fs.readFile, function (err, data) {
+
+		var returnData = {}
+		data.forEach(function(file,index){
+			var playerData = JSON.parse(file.toString());
+			console.log(playerData.length)
+
+			if(index === 0){
+				returnData.nba = percentageOlder(playerData, birthday);
+
+			}else if(index === 1){
+				returnData.nfl = percentageOlder(playerData, birthday);
 			}
 
-
-
-
-
 		})
-		console.log(youngerPlayers.length)
-		var percentageOlder = ((youngerPlayers.length/totalPlayers.length)*100).toFixed(2);
-		res.send({nba:percentageOlder});
-
+		res.send(returnData)
 	});
 
 
 });
-app.get('/nfl', function(req, res){
-	var birthday = new Date(req.query.birthday);
-	fs.readFile('./public/nfl_players.json', 'utf8', function (err,data) {
-		if (err) {
-			return console.log(err);
-		}
-		totalPlayers = JSON.parse(data);
-		youngerPlayers = [];
-		_.each(totalPlayers,function(player){
-			var playerBirthday = new Date(player);
-			if(birthday - playerBirthday < 0){
-				youngerPlayers.push(player)
-			}
 
-
-
-
-
-		})
-		console.log(totalPlayers.length)
-		var percentageOlder = ((youngerPlayers.length/totalPlayers.length)*100).toFixed(2);
-		res.send({nfl:percentageOlder});
-
-	});
-
-
-});
 
 
 
@@ -73,6 +60,6 @@ app.get('/nfl', function(req, res){
 
 
 var server = app.listen(port, function(){
-	console.log('listening on'+port);
+	console.log('listening on port '+port);
 });
 
